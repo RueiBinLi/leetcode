db908028dff615b877bbfec1905bff499ed2a1a7
Robby <robbylee0929@gmail.com>
2025-03-21 17:47:47 +0800

    0997_Simplify_Code

--- 0997_Find_the_Town_Judge/main.cpp
+++ 0997_Find_the_Town_Judge/main.cpp
@@ -6,14 +6,13 @@ using namespace std;
 class Solution {
     public:
         int findJudge(int n, vector<vector<int>>& trust) {
-            if(trust.size() == 0 && n == 1) return 1;
             vector<int> indegree(n+1, 0), outdegree(n+1, 0);
             for(int i = 0; i < trust.size(); i++) {
                 indegree[trust[i][1]]++;
                 outdegree[trust[i][0]]++;
             }
-            for(int i = 0; i < indegree.size(); i++) {
-                if(indegree[i] == n-1 & outdegree[i] == 0) return i;
+            for(int i = 1; i < indegree.size(); i++) {
+                if(indegree[i] == n-1 && outdegree[i] == 0) return i;
             }
             return -1;
         }
