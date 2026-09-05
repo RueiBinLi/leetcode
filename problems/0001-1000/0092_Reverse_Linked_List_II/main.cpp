ff8eda3778b0fa9ec2bbefd34af22a1e3331b2d7
rueibinli <s10959039@gm2.nutn.edu.tw>
2026-09-05 16:58:15 +0800

    Finish_20260905

--- problems/0001-1000/0092_Reverse_Linked_List_II/main.cpp
+++ problems/0001-1000/0092_Reverse_Linked_List_II/main.cpp
@@ -42,6 +42,28 @@ public:
         return newHead;
     }
 
+    ListNode* reverseBetween_0x32f(ListNode* head, int left, int right) { 
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* p0 = dummy;
+        for (int i = 0; i < left-1; i++) {
+            p0 = p0->next;
+        }
+
+        ListNode* pre = nullptr;
+        ListNode* cur = p0->next;
+        for (int i = 0; i < right-left+1; i++) {
+            ListNode* nxt = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+
+        p0->next->next = cur;
+        p0->next = pre;
+
+        return dummy->next;
+    }
+
     ListNode* createLinkedList(vector<int> v)  {
         ListNode* head = new ListNode(v[0]);
         ListNode* current = head;
